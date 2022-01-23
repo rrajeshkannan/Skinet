27dfda7f6b3ece0a0392a4d53a76742ab3e34859
Rajesh Kannan <rrajeshkannan@gmail.com>
2022-01-23 19:10:20 +0530

    introducing interfaces

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IProductPagination } from './models/product-pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe({
+        next: (response: IProductPagination) => {
+          console.log(response);
+          this.products = response.entities;
+        },
+        error: (err) => console.error(err)
+      });
+
+  }
 }
